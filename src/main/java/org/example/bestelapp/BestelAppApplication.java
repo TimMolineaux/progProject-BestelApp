df31e0e8b1e8be3aece36e05baadf0246caac809
TimMolineaux <tim.molineaux@gmail.com>
2025-06-13 10:31:36 +0200

    intiele commit

---
+++ src/main/java/org/example/bestelapp/BestelAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.bestelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestelAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestelAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bestelapp/BestelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bestelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestelAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
