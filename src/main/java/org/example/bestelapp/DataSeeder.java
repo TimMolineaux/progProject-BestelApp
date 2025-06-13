3f3b00267830924a879777cea0a22a5425bd2fd1
TimMolineaux <tim.molineaux@gmail.com>
2025-06-13 15:37:22 +0200

    Seeder voor default admin account

--- src/main/java/org/example/bestelapp/DataSeeder.java
+++ src/main/java/org/example/bestelapp/DataSeeder.java
@@ -2,19 +2,37 @@ package org.example.bestelapp;
 
 import org.example.bestelapp.Model.Role;
 import org.example.bestelapp.Model.RoleDAO;
+import org.example.bestelapp.Model.User;
+import org.example.bestelapp.Model.UserDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 @Configuration
 public class DataSeeder {
 
     @Bean
-    CommandLineRunner initRoles(RoleDAO roleDAO) {
+    CommandLineRunner initRoles(RoleDAO roleDAO, UserDAO userDAO) {
         return args -> {
             if (roleDAO.count() == 0){
                 roleDAO.save(new Role(0, "admin"));
                 roleDAO.save(new Role(0, "technieker"));
+            }
+            if (userDAO.count() == 0) {
+                Optional<Role> adminRole = roleDAO.findAll().stream()
+                        .filter(r -> r.getName().equalsIgnoreCase("admin"))
+                        .findFirst();
+
+                if (adminRole.isPresent()) {
+                    User admin = new User();
+                    admin.setEmail("admin@aquafin.be");
+                    admin.setPassword("admin123"); //deze nog versleutelen maybe
+                    admin.setRole(adminRole.get());
+
+                    userDAO.save(admin);
+                }
             }
         };
     }
