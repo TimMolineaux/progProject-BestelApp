30672f5de856a556414b34bc38a548820cdb627c
TimMolineaux <tim.molineaux@gmail.com>
2025-06-13 15:21:48 +0200

    User model + cotroller

--- src/main/java/org/example/bestelapp/Model/User.java
+++ src/main/java/org/example/bestelapp/Model/User.java
@@ -1,12 +1,8 @@
 package org.example.bestelapp.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Entity
 public class User {
@@ -15,14 +11,56 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(unique = true)
     @NotBlank
     @Email
-    private String mail;
+    private String email;
 
     @NotBlank
     private String password;
 
-    @NotBlank
-    private String role;
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
 
+    public User() {}
+
+    public User(int id, String email, String password, Role role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
